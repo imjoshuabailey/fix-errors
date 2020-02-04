57bf79ac6733d793b01c53ea50f704ce66e4bfcf
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-03 21:44:21 -0800

    fixed syntax on lesson8.js

--- lesson8.js
+++ lesson8.js
@@ -8,9 +8,9 @@ var people = [
 
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
-        let name = people[i].name
-        if (name > 50) {
-            console.log("My Name is " + name)
+        let age = people[i].age
+        if (age < 50) {
+            console.log("My Name is " + people[i].name)
         }
     }
 }
